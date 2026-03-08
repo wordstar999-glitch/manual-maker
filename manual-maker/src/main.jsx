52d423761df0125061850832a13f8a9f75c3568d
Ami <wordstar999@gmail.com>
2026-03-08 23:28:20 +0900

    새로운 UI 적용: 갤러리 선택, 순서 조정, 초성 검색

---
+++ manual-maker/src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js');
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
